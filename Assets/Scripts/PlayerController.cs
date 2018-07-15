0e9f19c241afa4c344ded3e3deba7754a4505556
Peter <37542469+ppak10@users.noreply.github.com>
2018-07-14 21:37:00 -0400

    Episode 1 Commit
    Commit Allows for basic map collision mapping and player movement and camera tracking

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour {
+	public LayerMask movementMask;
+	Camera cam;
+	PlayerMotor motor;
+
+	// Use this for initialization
+	void Start () {
+		cam = Camera.main;
+		motor = GetComponent<PlayerMotor>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0))
+		{
+			Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast(ray, out hit, 100, movementMask))
+			{
+				Debug.Log("Left-clicked: "+hit.collider.name+" "+hit.point);
+				motor.MoveToPoint(hit.point);
+				// Move our player to what we hit
+
+				// Stop focusing any objects
+			}
+		}
+
+		if (Input.GetMouseButtonDown(1))
+		{
+			Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast(ray, out hit, 100))
+			{
+				Debug.Log("Right-clicked: "+hit.collider.name+" "+hit.point);
+				// Check if there is interactable but have it focus
+			}
+		}
+	}
+}
