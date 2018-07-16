0d7a871e91df520fc0c40c9c3e20ea45cff088a7
Peter <37542469+ppak10@users.noreply.github.com>
2018-07-16 17:14:21 -0400

    Episode 8 Commit
    Commit adds models to armors and  pickup items.

--- Assets/EquipmentManager.cs
+++ Assets/EquipmentManager.cs
@@ -11,7 +11,12 @@ public class EquipmentManager : MonoBehaviour {
 		instance = this;
 	}
 
+	//#endregion
+
+	public Equipment[] defaultItems;
+	public SkinnedMeshRenderer targetMesh;
 	Equipment[] currentEquipment;
+	SkinnedMeshRenderer[] currentMeshes;
 
 	public delegate void OnEquipmentChanged (Equipment newItem, Equipment oldItem);
 	public OnEquipmentChanged onEquipmentChanged;
@@ -24,18 +29,16 @@ public class EquipmentManager : MonoBehaviour {
 		inventory = Inventory.instance;
 		int numSlots = System.Enum.GetNames(typeof(EquipmentSlot)).Length;
 		currentEquipment = new Equipment[numSlots];
+		currentMeshes = new SkinnedMeshRenderer[numSlots];
+
+		EquipDefaultItems();
 	}
 
 	public void Equip (Equipment newItem)
 	{
 		int slotIndex = (int)newItem.equipSlot;
 
-		Equipment oldItem = null;
-		if (currentEquipment[slotIndex] != null)
-		{
-			oldItem = currentEquipment[slotIndex];
-			inventory.Add(oldItem);
-		}
+		Equipment oldItem = Unequip(slotIndex);
 
 		if (onEquipmentChanged != null)
 		{
@@ -43,13 +46,26 @@ public class EquipmentManager : MonoBehaviour {
 		}
 
 		currentEquipment[slotIndex] = newItem;
+		SkinnedMeshRenderer newMesh = Instantiate<SkinnedMeshRenderer>(newItem.mesh);
+		newMesh.transform.parent = targetMesh.transform;
+
+		newMesh.bones = targetMesh.bones;
+		newMesh.rootBone = targetMesh.rootBone;
+		currentMeshes[slotIndex] = newMesh;
+
+		SetEquipmentBlendShapes(newItem, 100);
 	}
 
-	public void Unequip( int slotIndex)
+	public Equipment Unequip( int slotIndex)
 	{
 		if (currentEquipment[slotIndex]!=null)
 		{
+			if (currentMeshes[slotIndex] != null)
+			{
+				Destroy(currentMeshes[slotIndex].gameObject);
+			}
 			Equipment oldItem = currentEquipment[slotIndex];
+			SetEquipmentBlendShapes(oldItem, 0);
 			inventory.Add(oldItem);
 
 			currentEquipment[slotIndex] = null;
@@ -58,7 +74,9 @@ public class EquipmentManager : MonoBehaviour {
 			{
 					onEquipmentChanged.Invoke(null, oldItem);
 			}
+			return oldItem;
 		}
+		return null;
 	}
 
 	void UnequipAll()
@@ -66,6 +84,23 @@ public class EquipmentManager : MonoBehaviour {
 		for (int i = 0; i < currentEquipment.Length; i++)
 		{
 			Unequip(i);
+		}
+		EquipDefaultItems();
+	}
+
+	void SetEquipmentBlendShapes(Equipment item, int weight)
+	{
+		foreach (EquipmentMeshRegion blendShape in item.coveredMeshRegions)
+		{
+			targetMesh.SetBlendShapeWeight((int)blendShape, weight);
+		}
+	}
+
+	void EquipDefaultItems()
+	{
+		foreach (Equipment item in defaultItems)
+		{
+			Equip(item);
 		}
 	}
 
--- Assets/Scripts/Items/Equipment.cs
+++ Assets/Scripts/Items/Equipment.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class Equipment : Item {
 
 	public EquipmentSlot equipSlot;
+	public SkinnedMeshRenderer mesh;
+	public EquipmentMeshRegion[] coveredMeshRegions;
 
 	public int armorModifier;
 	public int damageModifier;
@@ -22,3 +24,4 @@ public class Equipment : Item {
 }
 
 public enum EquipmentSlot { Head, Chest, Legs, Weapon, Shield, Feet }
+public enum EquipmentMeshRegion {Legs, Arms, Toros}; // Corresponds  to body blendshapes.
