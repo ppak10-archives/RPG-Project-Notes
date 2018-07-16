f4f1531730658cba2d5fa5511d8e36ba7b643bbb
Peter <37542469+ppak10@users.noreply.github.com>
2018-07-16 17:57:59 -0400

    Episode 9 Commit
    Commit allows for user to utilize armor by adding player and character stats

---
+++ Assets/Scripts/Stats/PlayerStats.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : CharacterStats {
+
+	// Use this for initialization
+	void Start () {
+		EquipmentManager.instance.onEquipmentChanged += OnEquipmentChanged;
+	}
+
+	void OnEquipmentChanged (Equipment newItem, Equipment oldItem)
+	{
+		if (newItem != null)
+		{
+			armor.AddModifier(newItem.armorModifier);
+			damage.AddModifier(newItem.damageModifier);
+		}
+		if (oldItem != null)
+		{
+			armor.RemoveModifier(oldItem.armorModifier);
+			damage.RemoveModifier(oldItem.damageModifier);
+		}
+	}
+}
