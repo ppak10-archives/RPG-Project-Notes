fbc5e455606e60738733a5430119c419f73772fc
Peter <37542469+ppak10@users.noreply.github.com>
2018-08-18 00:27:42 -0400

    Comments Commit
    Commit adds comments to Stat.cs file in Stats

--- Assets/Scripts/Stats/Stat.cs
+++ Assets/Scripts/Stats/Stat.cs
@@ -8,22 +8,22 @@ public class Stat {
 	[SerializeField]
 	private int baseValue;
 
-	private List<int> modifiers = new List<int>();
+	private List<int> modifiers = new List<int>(); // Creates a private list of modifiers to add to base value of stat
 
-	public int GetValue()
+	public int GetValue() // Calculates the final value of the current stat with the modifiers list
 	{
-		int finalValue = baseValue;
-		modifiers.ForEach( x => finalValue += x);
-		return finalValue;
+		int finalValue = baseValue; // sets final values to orignal base value to start
+		modifiers.ForEach( x => finalValue += x); // for each value in modifier list as x, add to final value
+		return finalValue; // return modified final value
 	}
 
-	public void AddModifier (int modifier)
+	public void AddModifier (int modifier) // Adds modifier from equiped item to modifiers list
 	{
 		if (modifier != 0)
 			modifiers.Add(modifier);
 	}
 
-	public void RemoveModifier (int modifier)
+	public void RemoveModifier (int modifier) // Removes modifier from unequiped item to modifiers list
 	{
 		if (modifier != 0)
 			modifiers.Remove(modifier);
