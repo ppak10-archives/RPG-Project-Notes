24226a044a50de5ea8741b947e045a7e11941ab4
Peter <37542469+ppak10@users.noreply.github.com>
2018-08-21 14:55:40 -0400

    Update EnemyController.cs
    Commit adds additional comments

--- Assets/Scripts/Controllers/EnemyController.cs
+++ Assets/Scripts/Controllers/EnemyController.cs
@@ -13,35 +13,37 @@ public class EnemyController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		target = PlayerManager.instance.player.transform;
-		agent = GetComponent<NavMeshAgent>();
-		combat = GetComponent<CharacterCombat>();
+		target = PlayerManager.instance.player.transform; // sets the enemy's target and relavent data to player
+		agent = GetComponent<NavMeshAgent>(); // sets agent to navmesh agent in component
+		combat = GetComponent<CharacterCombat>(); // sets combat methods from character combat script
 	}
 
 	// Update is called once per frame
 	void Update () {
-		float distance = Vector3.Distance(target.position, transform.position);
+		float distance = Vector3.Distance(target.position, transform.position); // distance from player
 
-		if (distance <= lookRadius)
+		if (distance <= lookRadius) // if player is in enemy look radius
 		{
-			agent.SetDestination(target.position);
+			agent.SetDestination(target.position); // chase player
 
-			if (distance <= agent.stoppingDistance)
+			if (distance <= agent.stoppingDistance) // if target in stopplng distance radius of enemy
 			{
-				CharacterStats targetStats = target.GetComponent<CharacterStats>();
-				if (targetStats != null)
+				// Attack the target
+				CharacterStats targetStats = target.GetComponent<CharacterStats>(); // gets player stats data
+				if (targetStats != null) // if player's stats are not null (which should probably be never)
 				{
-					combat.Attack(targetStats);
+					combat.Attack(targetStats); // carry out attack by calling CharacterCombat script
 				}
 
 
-				// Attack the target
+				// Face the target
 				FaceTarget();
 
 			}
 		}
 	}
-
+	
+	// Rotate enemy to face player
 	void FaceTarget()
 	{
 		Vector3 direction = (target.position - transform.position).normalized;
